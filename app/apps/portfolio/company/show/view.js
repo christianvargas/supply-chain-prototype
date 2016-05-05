b4d7be2dcad82a92989dfb14330056e039c0c5a4
Christian Vargas <cvargas820@gmail.com>
2016-05-05 10:44:06 -0400

    Updates

--- app/apps/portfolio/company/show/view.js
+++ app/apps/portfolio/company/show/view.js
@@ -12,7 +12,152 @@ var Marionette = require('backbone.marionette'),
 
 
 var CompanyView = Marionette.ItemView.extend({
-  template: require('./templates/prototype/view4.hbs'),
+  template: require('./templates/prototype/overview.hbs'),
+
+  events: {
+    'click button[data-exposure-type]': 'changeView',
+  },
+
+  onShow: function(){
+    this.loadOverview();
+  },
+
+  changeView: function(e) {
+    $('button[data-exposure-type]').removeClass('active');
+    $(e.target).addClass('active');
+
+    var selected = $(e.target).data('exposure-type');
+    switch( selected ){
+      case 'overview':
+        this.loadOverview();
+        break;
+
+      case 'exposure':
+        this.loadExposure();
+        break;
+
+      case 'management':
+        this.loadManagement();
+        break;
+
+      case 'issues':
+        this.loadIssues();
+        break;
+    }
+  },
+
+  loadOverview: function(){
+    this.template = require('./templates/prototype/overview.hbs');
+    this.render();
+
+    // Build the chart
+    $('#revenue').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            height: 200,
+        },
+        title: {
+            text: null
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        credits: {
+          enabled: false,
+        },
+        exporting: {
+          enabled: false,
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        legend: {
+            // layout: 'vertical',
+        },
+        series: [{
+            name: 'Revenue',
+            colorByPoint: true,
+            data: [{
+                name: 'China',
+                y: 23,
+            }, {
+                name: 'Europe',
+                y: 8,
+            }, {
+                name: 'Corporate',
+                y: 40,
+            }]
+        }]
+    });
+
+    // Build the chart
+    $('#assets').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            height: 200,
+        },
+        title: {
+            text: null
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        credits: {
+          enabled: false,
+        },
+        exporting: {
+          enabled: false,
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        legend: {
+            // layout: 'vertical',
+        },
+        series: [{
+            name: 'Assets',
+            colorByPoint: true,
+            data: [{
+                name: 'China',
+                y: 23,
+            }, {
+                name: 'Europe',
+                y: 8,
+            }]
+        }]
+    });
+  },
+
+  loadExposure: function(){
+    this.template = require('./templates/prototype/exposure.hbs');
+    this.render();
+  },
+
+  loadManagement: function(){
+    this.template = require('./templates/prototype/management.hbs');
+    this.render();
+  },
+
 });
 
 module.exports = CompanyView;
